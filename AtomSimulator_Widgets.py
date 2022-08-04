Fixed normalization for single lattice, made widget boxes larger
If single lattice was chosen, the FFT wouldn't get normalized so I uncommented a line in the _Widgets.py file under the correct else statement

AtomSimulator_Widgets.py
@@ -2044,9 +2044,9 @@ class SimulatorWidget(QWidget):
 			elif self.lattice1 == 'Square':
 				self.Z, self.fftZ = squareatoms(self.pix, self.L, self.a, self.theta_im, self.e11, self.e12, self.e22)
 
-		# Normalize the FFTs to be between 0-1
-		# self.fftZ_norm = (self.fftZ - np.min(np.min((self.fftZ))))/(np.max(np.max(self.fftZ)) - np.min(np.min(self.fftZ)))
- 
+			# Normalize the FFTs to be between 0-1 (bc hexatoms only normalizes Z, moirelattice is what normalizes fftZ, but if you chose single lattice, moirelattice code isnt run. so need to normalize the FFT here)
+			self.fftZ = (self.fftZ - np.min(np.min((self.fftZ))))/(np.max(np.max(self.fftZ)) - np.min(np.min(self.fftZ)))
+	 
 
 
 

moirelattice.py
@@ -102,9 +102,9 @@ def moirelattice(pix, L, a1, a2, a3, moireBtn, lattice1, lattice2, lattice3, the
             Z3, fftZ3 = squareatoms(pix, L, a3, theta_tw23, f11, f12, f22)
       
       # Low pass filter the image if the button is checked:
-        if filter_bool == True:
+        if filter_bool == True: 
             Z = gaussian_filter((Z1 * Z2 * Z3 + Z1 + Z2 + Z3)/4, sigma,mode='mirror') # filter the stacked 3 lattices
-            fftZ = np.abs(npf.fftshift(npf.fft2(Z - np.mean(np.mean(Z)))))
+            fftZ = np.abs(npf.fftshift(npf.fft2(Z - np.mean(np.mean(Z))))) # If filtering, do not normalize FFT
             
         else:
             # Stack the 3 lattices to create moire superlattice